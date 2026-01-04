72ebeeab72f89c96141c40cf5086eb09a643fb46
Bhanu Tripathi <26444713+bhanu75@users.noreply.github.com>
2026-01-04 12:22:01 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import PhotoSequenceVideoScreen from './src/screens/PhotoSequenceVideoScreen';
+import { ThemeProvider } from './src/contexts/ThemeContext';
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <ThemeProvider>
+          <StatusBar style="light" />
+          <PhotoSequenceVideoScreen />
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
